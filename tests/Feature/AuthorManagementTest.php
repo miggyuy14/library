524780a1872767a0d29fcc4fbc826849f5828c9a
miggyuy14 <miggy.uy12@gmail.com>
2020-10-07 11:55:56 +0800

    adds author resource

---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Author;
+use Carbon\Carbon;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/author', [
+            'name' => 'Author name',
+            'dob' => '1995/10/22',
+        ]);
+        $author =  Author::all();
+
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('10/22/1995', $author->first()->dob->format('m/d/Y'));
+    }
+}
