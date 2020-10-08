8cd0674078cc528e0ccdfc27c70937bb751ae23d
miggyuy14 <miggy.uy12@gmail.com>
2020-10-08 11:53:21 +0800

    wip for authors

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -7,81 +7,21 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('authors.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store()
     {
-        Author::create(request()->only([
-            'name', 'dob',
-        ]));
+        Author::create($this->validateRequest());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Author $author)
+    protected function validateRequest()
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Author $author)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Author $author)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Author  $author
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Author $author)
-    {
-        //
+        return request()->validate([
+            'name' => 'required',
+            'dob' => 'required',
+        ]);
     }
 }
--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -2,11 +2,11 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
 use App\Author;
 use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AuthorManagementTest extends TestCase
 {
@@ -15,17 +15,36 @@ class AuthorManagementTest extends TestCase
     /** @test */
     public function an_author_can_be_created()
     {
-        $this->post('/author', [
-            'name' => 'Author name',
-            'dob' => '1995/10/22',
-        ]);
-        $author =  Author::all();
+        $this->post('/authors', $this->data());
+
+        $author = Author::all();
 
         $this->assertCount(1, $author);
         $this->assertInstanceOf(Carbon::class, $author->first()->dob);
-        $this->assertEquals('10/22/1995', $author->first()->dob->format('m/d/Y'));
+        $this->assertEquals('1988/14/05', $author->first()->dob->format('Y/d/m'));
     }
 
-   
+    /** @test */
+    public function a_name_is_required()
+    {
+        $response = $this->post('/authors', array_merge($this->data(), ['name' => '']));
 
+        $response->assertSessionHasErrors('name');
+    }
+
+    /** @test */
+    public function a_dob_is_required()
+    {
+        $response = $this->post('/authors', array_merge($this->data(), ['dob' => '']));
+
+        $response->assertSessionHasErrors('dob');
+    }
+
+    private function data()
+    {
+        return [
+            'name' => 'Author Name',
+            'dob' => '05/14/1988',
+        ];
+    }
 }
